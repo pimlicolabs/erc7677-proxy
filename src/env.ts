49bc85c9ec7ce46c01559cb9af63acb17d4d2e50
Kristof Gazso <kristof.gazso@gmail.com>
2024-05-16 17:26:23 +0100

    test

--- src/env.ts
+++ src/env.ts
@@ -9,18 +9,18 @@ export const env = createEnv({
 		PORT: z
 			.string()
 			.transform((val) => Number(val))
-			.default("3000"),
-		PAYMASTER_SERVICE_URL: z.string().url(),
-		ENTRYPOINT_V06_ENABLED: z.boolean().default(true),
-		ENTRYPOINT_V07_ENABLED: z.boolean().default(true),
+			.default("3000").describe("The port the server will listen on"),
+		PAYMASTER_SERVICE_URL: z.string().url().describe("The URL of the paymaster service"),
+		ENTRYPOINT_V06_ENABLED: z.boolean().default(true).describe("Whether the v0.6 entrypoint is enabled"),
+		ENTRYPOINT_V07_ENABLED: z.boolean().default(true).describe("Whether the v0.7 entrypoint is enabled"),
 		CHAIN_ID_WHITELIST: z
 			.string()
 			.transform((val) => val.split(",").map((v) => Number(v)))
-			.optional(),
+			.optional().describe("The chain IDs that are supported (defaults to all chains)"),
 		PIMLICO_SPONSORSHIP_POLICY_IDS: z
 			.string()
 			.transform((val) => val.split(","))
-			.optional(),
+			.optional().describe("If using Pimlico paymasters, then a comma separated list of Pimlico sponsorship policy IDs to check against on each request"),
 	},
 	runtimeEnv: process.env,
 	emptyStringAsUndefined: true,
